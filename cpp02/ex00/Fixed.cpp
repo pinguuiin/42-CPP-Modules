198140ca6637d24539d2cdde860b0717a3dacc3d
pinguuiin <yuping199407@hotmail.com>
2026-01-06 02:36:26 +0200

    Add cpp02 ex00

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -17,8 +17,20 @@ Fixed::Fixed() : raw_(0)
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &other)
+Fixed::Fixed(const Fixed &other) : raw_(other.raw_)
 {
-	raw
 	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		raw_ = other.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
 }
