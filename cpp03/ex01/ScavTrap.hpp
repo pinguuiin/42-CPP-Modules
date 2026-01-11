1d6e18c2cd3f73eaf94bf8d6b1ffb13bf7000212
pinguuiin <yuping199407@hotmail.com>
2026-01-11 03:12:02 +0200

    cpp03 ex01 change functions to virtual when there is an override

--- cpp03/ex01/ScavTrap.hpp
+++ cpp03/ex01/ScavTrap.hpp
@@ -10,9 +10,9 @@ class ScavTrap : public ClapTrap {
 		ScavTrap(std::string name);
 		ScavTrap(const ScavTrap &other);
 		ScavTrap &operator=(const ScavTrap &other);
-		~ScavTrap();
+		~ScavTrap() override;
 
-		void attack(const std::string &target);
+		void attack(const std::string &target) override;
 		void guardGate();
 };
 
--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -5,15 +5,17 @@ int	main()
 	ScavTrap a("McDonald");
 	ScavTrap b("Hesburger");
 	ScavTrap c(a);
-	ScavTrap d;
+	ClapTrap *d = &b;
 	std::cout << std::endl;
 
+	d->attack("mayo");
 	b.attack("Hesburger");
 	b.takeDamage(20);
 	b.beRepaired(10);
+	b.guardGate();
 	std::cout << std::endl;
 
-	for (int i = 0; i < 60; ++i) {
+	for (int i = 0; i < 55; ++i) {
 		a.attack("Hesburger");
 		b.takeDamage(20);
 	}
